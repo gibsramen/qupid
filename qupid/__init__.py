05b845e8fe7ffe4224772528f07256db0fda7782
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-02-18 15:51:44 -0800

    post 0.0.1 updates

--- qupid/__init__.py
+++ qupid/__init__.py
@@ -1,6 +1,6 @@
 from .matching import CaseMatch, match_by_single, match_by_multiple
 
 
-__version__ = "0.0.1"
+__version__ = "0.0.2dev"
 
 __all__ = ["CaseMatch", "match_by_single", "match_by_multiple"]
