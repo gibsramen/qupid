9bdfe4db38e4dd259e3a8d2e5b139fdef921de97
Gibraan Rahman <grahman@eng.ucsd.edu>
2022-06-06 11:02:49 -0700

    updates

--- qupid/__init__.py
+++ qupid/__init__.py
@@ -1,7 +1,7 @@
 from .casematch import (CaseMatchOneToMany, CaseMatchOneToOne,
                         match_by_single, match_by_multiple)
 
-__version__ = "0.0.2dev"
+__version__ = "0.0.2"
 
 __all__ = ["CaseMatchOneToMany", "CaseMatchOneToOne",
            "match_by_single", "match_by_multiple"]
